4f5f7afacd45dc0f1a3384b04f49d660ef20abfd
AYMAN2448 <aloby2448@gmail.com>
2026-05-22 15:51:14 +0200

    Add files via upload

---
+++ backend/src/bull.ts
@@ -0,0 +1,7 @@
+// backend/src/config/bull.ts
+import { Queue, Worker } from 'bullmq';
+import { redis } from './redis';
+
+export const createQueue = (name: string) => new Queue(name, { connection: redis });
+export const createWorker = (name: string, processor: any) =>
+  new Worker(name, processor, { connection: redis });
